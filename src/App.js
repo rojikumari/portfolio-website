e5d7e9547448dec3e4bc7916aa0cd00e644f88df
Kumari Roji <rojikumari8411@gmail.com>
2024-02-21 05:46:20 +0530

    feat-contact

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import HeroSection from './Components/HeroSection';
 import About from './Components/About';
 import Project from './Components/Projects';
 import Skills from './Components/Skills';
+import Contact from './Components/Contact';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
        <About/>
        <Project/>
        <Skills/>
+       <Contact/>
     </div>
   );
 }
